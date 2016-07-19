61a9201f045ad8155abe22a97c5344e3f056fc5b
sjayathilaka <sohanyajayathilaka@yahoo.com>
2016-07-19 15:06:46 +0530

    Refacteding the unwanted comments it's one of the bad smell...

--- suhanya-1432284/src/sj1432284/PictureFrame.java
+++ suhanya-1432284/src/sj1432284/PictureFrame.java
@@ -91,16 +91,6 @@ public class PictureFrame {
       g.setColor(Color.YELLOW);
       g.fillRect(0, 0, getWidth(), getHeight());
 
-      // numbaz(g);
-      //
-      // if (master!=null && master.orig != null) {
-      // drawRoll(g, master.orig);
-      // }
-      // if (reroll != null) {
-      // drawReroll(g, reroll);
-      // }
-      //
-      // drawGrid(g);
       Location l = new Location(1,2);
 
       if (master.mode == 1) {
